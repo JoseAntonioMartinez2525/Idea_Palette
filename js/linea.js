ed12dda773c39b95a3c5be7aa1f22fc29dafe4af
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2023-05-26 19:57:06 -0700

    Actualizar cambios

--- js/linea.js
+++ js/linea.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CustomLine {
   constructor(x1, y1, x2, y2, color) {
     this.x1 = x1;
@@ -12,19 +11,4 @@ class CustomLine {
     stroke(this.strokeColor); // Aplicar el color del contorno
     line(this.x1, this.y1, this.x2, this.y2);
   }
-=======
-class CustomLine {
-  constructor(x1, y1, x2, y2, color) {
-    this.x1 = x1;
-    this.y1 = y1;
-    this.x2 = x2;
-    this.y2 = y2;
-    this.strokeColor = color; // Establecer el color del contorno de la nueva línea
-  }
-
-  display() {
-    stroke(this.strokeColor); // Aplicar el color del contorno
-    line(this.x1, this.y1, this.x2, this.y2);
-  }
->>>>>>> 409f79b9e60f36ef26371eae73d5951fb5de3351
 }
--- js/rect.js
+++ js/rect.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class CustomRectangle {
   constructor(x, y, width, height, color) {
     this.x = x;
@@ -21,28 +20,3 @@ class CustomRectangle {
 }
 
 
-=======
-class CustomRectangle {
-  constructor(x, y, width, height, color) {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-    this.color = color;
-    this.strokeColor = 0; // Color de trazo predeterminado
-  }
-
-  display() {
-    fill(this.color);
-    stroke(this.strokeColor); // Utilizar el color de trazo definido
-    rect(this.x, this.y, this.width, this.height);
-  }
-
-  setStrokeColor(color) {
-    this.strokeColor = color;
-  }
-}
-
-
-
->>>>>>> 409f79b9e60f36ef26371eae73d5951fb5de3351
