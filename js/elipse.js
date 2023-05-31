9b02c8fc6e525d71ce5595bacc351f646f7a9df3
JoseAntonioMartinez2525 <bubbleteahyung@gmail.com>
2023-05-30 19:14:59 -0700

    Actualizando cambios al repositorio correcto

---
+++ js/elipse.js
@@ -0,0 +1,15 @@
+class CustomEllipse {
+  constructor(x, y, width, height, color) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  display() {
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.width, this.height);
+  }
+}
---
+++ js/texto.js
@@ -0,0 +1,15 @@
+class CustomTexto {
+  constructor(texto, x, y, color) {
+    this.texto = texto;
+    this.x = x;
+    this.y = y;
+    this.strokeColor = color;
+  }
+
+  display() {
+    stroke(this.strokeColor);
+    noFill();
+    text(this.texto, this.x, this.y);
+  }
+}
+
